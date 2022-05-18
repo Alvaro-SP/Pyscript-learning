dc9420cdc75caddc630a757b2d645f32fa96f7fb
Alvaro-SP <socop2412@gmail.com>
2022-05-18 15:49:55 -0600

    ADDED new commit to github

--- commit Compi.bat
+++ commit Compi.bat
@@ -2,8 +2,9 @@
 @REM cd "C:\Users\dell\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\5 Q U I N T O S E MESTRE\LAB. Compi 1\PROYECTO 2\GITHUB\-OLC1-Proyecto2_202000194"
 
 @REM PC DESKTOP
-cd "C:\Users\spuser\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\5 Q U I N T O S E MESTRE\LAB. Compi 1\PROYECTO 2\GITHUB\-OLC1-Proyecto2_202000194"
+
+cd "C:\Users\spuser\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\Escritorio\PYSCRIPT\Pyscript-learning"
 git add .
-git commit -m "commit ULTIMO Gracias a DIOS !!."
+git commit -m "ADDED new commit to github"
 git push
 pause
